ee65d2783042db9fbf6c8d3e476b219c56e2690a
darshan051292 <darshan051292@gmail.com>
2022-11-11 17:24:03 +0530

    "tenthcommit"

--- app.py
+++ app.py
@@ -72,12 +72,13 @@ def resultfunction():
             search_link = "https://ineuron.ai/category/" + formatted_search
             logging.info(search_link)
             try:
-                chrome_options = webdriver.ChromeOptions()
-                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-                chrome_options.add_argument("--headless")
-                chrome_options.add_argument("--disable-dev-shm-usage")
-                chrome_options.add_argument("--no-sandbox")
-                browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+                #chrome_options = webdriver.ChromeOptions()
+                #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                #chrome_options.add_argument("--headless")
+                #chrome_options.add_argument("--disable-dev-shm-usage")
+                #chrome_options.add_argument("--no-sandbox")
+                #browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+                browser = webdriver.Chrome(executable_path=r"C:\Users\Darshan\PycharmProjects\coursescraper\chromedriver.exe")
                 browser.get(search_link)
                 courses_html = browser.page_source
             except Exception as e:
